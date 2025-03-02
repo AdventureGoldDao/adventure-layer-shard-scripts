1f634ebb52c8797fd84fb4c6244c5bd6402669ef
adam <xy849607831@gmail.com>
2025-03-02 15:08:14 +0800

    build:up shard-node scripts

--- shard-node-demo/scripts/consts.ts
+++ shard-node-demo/scripts/consts.ts
@@ -3,3 +3,7 @@ export const chainidstore = "/config/chainidstore";
 export const l1passphrase = "passphrase";
 export const configpath = "/config";
 export const tokenbridgedatapath = "/tokenbridge-data";
+// Not secure. Do not use for production purposes
+export const l1mnemonic = process.env.L1_MNEMONIC as string;
+
+export const ARB_OWNER = "0x0000000000000000000000000000000000000070";
--- shard-node-demo/scripts/index.ts
+++ shard-node-demo/scripts/index.ts
@@ -3,50 +3,79 @@ import Yargs from "yargs/yargs";
 import { stressOptions } from "./stress";
 import { redisReadCommand, redisInitCommand } from "./redis";
 import { getChainIdStoreCommand } from "./api";
-import {writeConfigCommand, writePrysmCommand, writeL2ChainConfigCommand, writeAccountsCommand} from "./config";
+import { writeConfigCommand, writeGethGenesisCommand, writePrysmCommand, writeL2ChainConfigCommand, writeL3ChainConfigCommand, writeL2DASCommitteeConfigCommand, writeL2DASMirrorConfigCommand, writeL2DASKeysetConfigCommand } from "./config";
 import {
-    bridgeFundsCommand,
-    createERC20Command,
-    transferERC20Command,
-    sendL1Command,
-    sendL2Command,
-    sendRPCCommand,
-    waitForSyncCommand,
+    printAddressCommand,
+    namedAccountHelpString,
+    writeAccountsCommand,
+    printPrivateKeyCommand, namedAddress,
+} from "./accounts";
+import {
+  bridgeFundsCommand,
+  bridgeNativeTokenToL2Command,
+  bridgeToL3Command,
+  createERC20Command,
+  createWETHCommand,
+  transferERC20Command,
+  sendL1Command,
+  sendL2Command,
+  sendL3Command,
+  sendRPCCommand,
+  setValidKeysetCommand,
+  waitForSyncCommand,
+  transferL3ChainOwnershipCommand,
+  createFeeTokenPricerCommand,
 } from "./ethcommands";
 
-
 async function main() {
-    await Yargs(hideBin(process.argv))
-        .options({
-            redisUrl: { string: true, default: "redis://redis:6379" },
-            l2url: { string: true, default: process.env.L2_RPC_URL },
-            shardUrl: { string: true, default: "ws://sequencer:8548" },
-            validationNodeUrl: { string: true, default: "ws://validation_node:8549" },
-            shardOwner: { string: true, default: process.env.SHARD_ADMIN_ADDRESS },
-        })
-        .options(stressOptions)
-        .command(bridgeFundsCommand)
-        .command(createERC20Command)
-        .command(transferERC20Command)
-        .command(sendL1Command)
-        .command(sendL2Command)
-        .command(sendRPCCommand)
-        .command(writeAccountsCommand)
-        .command(writeConfigCommand)
-        .command(writeL2ChainConfigCommand)
-        .command(writePrysmCommand)
-        .command(redisReadCommand)
-        .command(redisInitCommand)
-        .command(getChainIdStoreCommand)
-        .command(waitForSyncCommand)
-        .strict()
-        .demandCommand(1, "a command must be specified")
-        .help().argv;
+  await Yargs(hideBin(process.argv))
+    .options({
+      redisUrl: { string: true, default: "redis://redis:6379" },
+      l1url: { string: true, default: process.env.L1_RPC_URL as string },
+      l2url: { string: true, default: "ws://sequencer:8548" },
+      l3url: { string: true, default: "ws://l3node:3348" },
+      validationNodeUrl: { string: true, default: "ws://validation_node:8549" },
+      l2owner: { string: true, default: namedAddress("l2owner") },
+      committeeMember: { string: true, default: "not_set" },
+    })
+    .options(stressOptions)
+    .command(bridgeFundsCommand)
+    .command(bridgeToL3Command)
+    .command(bridgeNativeTokenToL2Command)
+    .command(createERC20Command)
+    .command(createFeeTokenPricerCommand)
+    .command(createWETHCommand)
+    .command(transferERC20Command)
+    .command(sendL1Command)
+    .command(sendL2Command)
+    .command(sendL3Command)
+    .command(sendRPCCommand)
+    .command(setValidKeysetCommand)
+    .command(getChainIdStoreCommand)
+    .command(transferL3ChainOwnershipCommand)
+    .command(writeConfigCommand)
+    .command(writeGethGenesisCommand)
+    .command(writeL2ChainConfigCommand)
+    .command(writeL3ChainConfigCommand)
+    .command(writeL2DASCommitteeConfigCommand)
+    .command(writeL2DASMirrorConfigCommand)
+    .command(writeL2DASKeysetConfigCommand)
+    .command(writePrysmCommand)
+    .command(writeAccountsCommand)
+    .command(printAddressCommand)
+    .command(printPrivateKeyCommand)
+    .command(redisReadCommand)
+    .command(redisInitCommand)
+    .command(waitForSyncCommand)
+    .strict()
+    .demandCommand(1, "a command must be specified")
+    .epilogue(namedAccountHelpString)
+    .help().argv;
 }
 
 main()
-    .then(() => process.exit(0))
-    .catch((error) => {
-        console.error(error);
-        process.exit(1);
-    });
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
